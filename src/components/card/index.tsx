d1f760998c9e1e8f80103331f1665c439f880d69
Federico Luzzi <fedeluzzi00@gmail.com>
2021-10-23 21:06:17 +0200

    feat: increase card visibility over white background

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,3 +1,4 @@
+import { transparentize } from 'polished'
 import { Card as RebassCard } from 'rebass'
 import styled from 'styled-components'
 
@@ -7,10 +8,11 @@ export const Card = styled(RebassCard)<{
   onClick?: () => void
   disabled?: boolean
 }>`
-  padding: 64px 80px;
-  width: 684px;
-  height: 300px;
+  padding: 52px 82px;
+  width: 664px;
+  height: 272px;
   background: ${(props) => props.theme.background};
-  box-shadow: 0px 13px 80px rgba(50, 47, 82, 0.04);
+  box-shadow: 0px 13px 80px rgba(50, 47, 82, 0.1);
   border-radius: 40px;
+  border: solid 1px ${(props) => transparentize(0.5, props.theme.border)};
 `
