f0c53ebc4e218f96c5165fdabb3437e1838c78db
Federico Luzzi <fedeluzzi00@gmail.com>
2021-10-24 11:30:32 +0200

    feat: change footer text color

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,15 @@
 import { Flex, Box, Text } from 'rebass'
+import styled from 'styled-components'
+
+const ColoredText = styled(Text)`
+  color: ${(props) => props.theme.contentSecondary};
+`
 
 export const Footer = () => {
   return (
     <Flex width="100%" my="40px" justifyContent="center" alignItems="center">
       <Box>
-        <Text fontSize="12px">DXDAO | ETHLISBON 2021</Text>
+        <ColoredText fontSize="12px">DXDAO | ETHLISBON 2021</ColoredText>
       </Box>
     </Flex>
   )
